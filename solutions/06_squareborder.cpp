7d5975caa081853ac6b07bf0010cc3c420ec97da
carlos <carlos@carlos-VirtualBox.(none)>
2014-02-10 19:33:20 +0100

    correcion

--- solutions/06_squareborder.cpp
+++ solutions/06_squareborder.cpp
@@ -12,12 +12,15 @@ int main(int argv,char *argc[]){
 	for(int fila=0;fila<altura;fila++){
 
 		for(int col=0;col<altura;col++)
-        if( fila ==  col )         
-			printf(" ");
-        else
-                        printf("*");
+			if( fila == 0 || col == 0 || fila == altura -1 || col == altura -1 )         
+				printf("*");
+			else
+				printf(" ");
+
+
 		printf("\n");
 	}
+
 	return EXIT_SUCCESS;
 }
 
