2313a54009190f0cbd4b5d2766088970cf6be8e8
carlos <carlos@dam1-pc00.(none)>
2014-02-11 11:09:41 +0100

    html

---
+++ solutions/07_adding.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+double add(double operando1, double operando2){
+
+
+	return 3.7;
+}
+
+
+int main(int argv,char *argc[]){
+
+	double param1, param2;
+
+	param1 = param2 = 73.5;
+
+
+	printf("%.2lf\n", add(param1, param2 ) );
+
+	return EXIT_SUCCESS;
+}
