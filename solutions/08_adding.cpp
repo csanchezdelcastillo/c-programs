0f4a9ae36e990928097cbcc39ec1a2e1da4eeba7
carlos <carlos@dam1-pc00.(none)>
2014-02-12 13:32:17 +0100

    exercise 08 incomplete

---
+++ solutions/08_adding.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc,char *argv[]){
+
+
+  //para introducir todos los numeros que queramos "argc-1"
+
+ double numeros[argc-1]
+        contador_de_numeros = argc-1,
+        resultado = 0;
+  //metemos los datos en una matriz
+
+ for(int contador=0; contador<contador_
