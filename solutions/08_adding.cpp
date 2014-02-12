035f0b017127fec4386e4ca5fe1e2dcc15ed5776
carlos <carlos@carlos-VirtualBox.(none)>
2014-02-12 18:58:22 +0100

    correcion

--- solutions/08_adding.cpp
+++ solutions/08_adding.cpp
@@ -1,15 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+double add(double numeros[], double cuantos_numeros){
+
+	double resultado = 0;
+
+	for(int contador=0; contador < cuantos_numeros; contador++)
+
+		resultado+=numeros[contador];
+	return resultado;
+}
 
 int main(int argc,char *argv[]){
 
 
-  //para introducir todos los numeros que queramos "argc-1"
+	//para introducir todos los numeros que queramos "argc-1"
 
- double numeros[argc-1]
-        contador_de_numeros = argc-1,
-        resultado = 0;
-  //metemos los datos en una matriz
+	double numeros[argc-1],
+		cuantos_numeros = argc-1,
+				resultado = 0;
 
- for(int contador=0; contador<contador_
+	//metemos todos los datos en una matriz
+
+	for(int contador=0; contador<cuantos_numeros;contador++){
+
+		numeros[contador]=atof(argv[contador+1]);
+	}
+
+	//llamada al a funcion.
+
+	resultado = add(numeros, cuantos_numeros);
+
+	printf(" %.2lf\n",resultado);
+
+	return EXIT_SUCCESS;
+}
