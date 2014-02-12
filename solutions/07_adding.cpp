83072d3f51dbb6e129763b96b46d086178d2f634
carlos <carlos@dam1-pc00.(none)>
2014-02-12 12:49:28 +0100

    difficult just for me

--- solutions/07_adding.cpp
+++ solutions/07_adding.cpp
@@ -3,19 +3,27 @@
 
 double add(double operando1, double operando2){
 
+	//inicializamos el resultado a 0, despues decimos que resultado es igual a la suma de operandos y devolvemos el valor a resultado.
 
-	return 3.7;
+	double resultado = 0; 
+	resultado = operando1 + operando2;
+	return resultado;
 }
 
 
-int main(int argv,char *argc[]){
-
-	double param1, param2;
-
-	param1 = param2 = 73.5;
+int main(int argc,char *argv[]){
 
 
-	printf("%.2lf\n", add(param1, param2 ) );
+	//Atof para convertir los datos en tipo float.
+
+	double operando1 = atof(argv[1]),
+	       operando2 = atof(argv[2]),
+	       resultado = 0;
+	//La variable resultado tiene el valor que le devuelva la funcion add.
+
+	resultado = add(operando1, operando2);
+
+	printf("%.2lf\n", resultado );
 
 	return EXIT_SUCCESS;
 }
