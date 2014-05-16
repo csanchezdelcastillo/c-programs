4756b6f6f0ec4394c29ff8fe642a977871d47c86
Carlos Sanchez del Castillo <csanchez@dam1-pc00.(none)>
2014-05-16 12:48:31 +0200

    changes

---
+++ javascript/menu/main.js
@@ -0,0 +1,13 @@
+function: enlight(element, event){
+
+ event = event || window.event
+	  var header = element.getElemtByTagName("h1")[0]
+	  header.style.backgroundColor = "Cyan"
+
+	  }
+function vanish(element. event) {
+
+ event = event || window.event
+	  var header = element.firstChild
+	  header.style.background = "black"
+}
