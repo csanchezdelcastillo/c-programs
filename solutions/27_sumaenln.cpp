cd495bcafbaabcd39c6cc9a9400e748aa25603a0
Administrador <administrador@ubuntu.(none)>
2014-11-19 23:14:00 +0100

    nuevo ejercicio

--- solutions/27_sumaenln.cpp
+++ solutions/27_sumaenln.cpp
@@ -4,23 +4,22 @@
 
 int main(int argc,char *argv[]){
 
-    int numero1 =atoi(argv[1]);
 
-    
-    int suma; 
 
-    
-    
-    
-   //** printf("%i + %i = %i\n", numero1, numero2, numero1 + numero2 );**//
-    for( int acumulador = 0 ; acumulador<= argc ;acumulador ++)
+	int suma; 
 
-    	suma += numero1;
-        (argv[1++]);
 
-    printf("El resultado es = %i\n", suma);
 
-    
 
-    return EXIT_SUCCESS;
+	//** printf("%i + %i = %i\n", numero1, numero2, numero1 + numero2 );**//
+	for( int acumulador = 0 ; acumulador< argc ;acumulador ++)
+
+		suma += atoi(argv[acumulador]);
+
+
+	printf("El resultado es = %i\n", suma);
+
+
+
+	return EXIT_SUCCESS;
 }
