703215c25f11ddde5f330c449036259ded617ef7
Carlos Sánchez Del Castillo <csanchez@dam1-pc00.(none)>
2015-02-09 10:39:52 +0100

    cambios

--- solutions/67_lista_de_nombres.cpp
+++ solutions/67_lista_de_nombres.cpp
@@ -1,22 +1,40 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
 
 int main(int argc,char *argv[]){
 
-	const char *lista[10];
-	int b =0;
+        char **lista = NULL;
+	int n_palabras = 0;
+	char buffer[100];
+	//preguntar la palabra
+	//contar las letras
+	//incrementar el directorio en uno
+	//reservar para la cantidad de caracteres
+	//copiar la palbra desde el buffer
+
 	do{
-	    b++;
-	    printf("introduce tu nombre:\n");
-	    scanf("%s", lista);
-	    
-	}while(b<10);
+	    n_palabras++;
+	    printf("introduce tu nombre: ");
+	    scanf("%s", buffer);
+	    lista = (char **) realloc (lista, n_palabras * sizeof (char *));
+	    lista[n_palabras-1] = (char *) malloc(strlen(buffer) + 1);
+	    strncpy(lista[n_palabras-1],buffer,strlen(buffer)+1); 
+	    //strncpy(dondeloquierescopiar, de donde lo copias, y el tamaño k va a tener)
+	    //strlen de buffer cuanto el numero de lo que haya en este caso caracteres)
+	}while(n_palabras< 5);
 
-	for(int i=1;i<10;i++)
-	printf("Nombre: %s", lista[i]);
+	for(int i=0;i<n_palabras;i++)
+  	 printf("Nombre: %s\n", lista[i]);
 
+	for(int a=0;a<n_palabras;a++)
+	    free(lista[a]);
 
-
+        free(lista);
+	// free() libera el array creado
+        //string case compare ?¿?¿
+	
     return EXIT_SUCCESS;
 }
